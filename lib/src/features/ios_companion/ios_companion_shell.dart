25c0bdc0b39a926e530cf28633ac9497d926dabf
david08170129 <david08170129@gmail.com>
2026-05-14 00:13:33 +1000

    Open scanned QR links

--- lib/src/features/ios_companion/ios_companion_shell.dart
+++ lib/src/features/ios_companion/ios_companion_shell.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../theme/gmp_colors.dart';
 import '../../widgets/gmp_logo.dart';
@@ -243,11 +244,14 @@ class _ScannerPlaceholder extends StatelessWidget {
                 final List<Barcode> barcodes = capture.barcodes;
 
                 for (final barcode in barcodes) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-  SnackBar(
-    content: Text('QR Found: ${barcode.rawValue}'),
-  ),
-);
+                 
+                final raw = barcode.rawValue;
+
+if (raw != null) {
+  final uri = Uri.parse(raw);
+
+  launchUrl(uri);
+} 
                 }
               },
             ),
